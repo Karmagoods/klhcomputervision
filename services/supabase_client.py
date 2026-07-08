ca8f49d7072dbd2ce9d1149f580ccab6a784fc91
KLHcreate <karlheydenrych83@gmail.com>
2026-07-08 14:13:25 +0200

    supa

---
+++ services/supabase_client.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from supabase import Client, create_client
+
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
+
+supabase: Client = create_client(
+    SUPABASE_URL,
+    SUPABASE_KEY,
+)
